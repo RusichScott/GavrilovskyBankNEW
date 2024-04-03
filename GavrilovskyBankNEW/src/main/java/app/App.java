d89936448bcf431ff0eda4cfe662dc1a5bbe3699
RusichScott <143874084+RusichScott@users.noreply.github.com>
2024-04-03 10:04:39 +0300

    Add files via upload

---
+++ GavrilovskyBankNEW/src/main/java/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+import bank.ATM;
+import bank.Bank;
+import bank.Card;
+import data.BankAccount;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        ATM atm = new ATM();
+        atm.start();
+
+    }
+}
